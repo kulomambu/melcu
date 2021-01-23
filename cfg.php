9ed7e47dbd8070966201edbfdb0e076861268265
kulomambu <77854129+kulomambu@users.noreply.github.com>
2021-01-24 06:12:55 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,15 @@
+<?php
+
+//BAHAN YANG PERLU DIINSTAL DI TERMUX
+//[1]. pkg install php
+//[2]. pkg install tesseract
+//[3]. pkg install imagemagick
+//Sampai Sini sudah Paham Kan 
+//Jangan Lupa Subscribe Chanel Penghasil Gratisa
+
+
+$ua = "Mozilla/5.0 (Linux; Android 9; ASUS_X00TD) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Mobile Safari/537.36";
+
+$PHPSESSID = "PHPSESSID=g0qdhj6pis3q2u22tp2jliqd6i";
+
+$get_id = "get_id=129311";
